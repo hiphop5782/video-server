0c4c41b7dc39f525388966760bb314921cd1e50c
hiphop5782 <zdlsz@hanmail.net>
2023-03-23 21:42:21 +0900

    JSP 적용

---
+++ video/src/main/java/com/hacademy/video/controller/ViewController.java
@@ -0,0 +1,14 @@
+package com.hacademy.video.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+
+	@RequestMapping("/view")
+	public String view() {
+		return "/WEB-INF/views/home.jsp";
+	}
+	
+}
